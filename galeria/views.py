3e35a0e1cc7549a463d9212d99b3d1ce57635099
Matheus (Manasi) <matheusnascimentox725@gmail.com>
2025-06-28 21:23:54 -0300

    Criando modelos de fotografias

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from galeria.models import Photograph
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    photographs = Photograph.objects.all()
+    return render(request, 'galeria/index.html', {"cards": photographs})
 
 def imagem(request):
+    
     return render(request, 'galeria/imagem.html')
